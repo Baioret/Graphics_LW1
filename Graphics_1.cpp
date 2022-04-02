2e7a3c46c8f4d926698f52370b0f6e492ed1e167
Baioret <ranreo.reo@gmail.com>
2022-04-02 14:32:25 +0500

    Tutorial 01: creating window. Done

---
+++ Graphics_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "C:\SRC\vcpkg\packages\freeglut_x64-windows\include\GL\glut.h"
+
+int main(int argc, char** argv)
+{
+    void RenderSceneCB();
+    void glutInit(int* argcp, char** argv);
+
+    glutInit(&argc, argv);
+
+    glutCreateWindow("Tutorial 01");
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(1024, 768);
+    glutDisplayFunc(RenderSceneCB);
+
+    glutMainLoop();
+
+    return 0;
+}
+
+void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
