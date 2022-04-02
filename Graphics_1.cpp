def509031bf78406cf571362711d97fb318f3134
Baioret <ranreo.reo@gmail.com>
2022-04-02 16:07:06 +0500

    Tutorial 02: Creating vertex. Done

--- Graphics_1.cpp
+++ Graphics_1.cpp
@@ -1,5 +1,9 @@
 ﻿#include <iostream>
+#include "C:\SRC\vcpkg\packages\glew_x64-windows\include\GL\glew.h"
 #include "C:\SRC\vcpkg\packages\freeglut_x64-windows\include\GL\glut.h"
+#include "math_3d.h"
+
+GLuint VBO;
 
 int main(int argc, char** argv)
 {
@@ -12,8 +16,23 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(1024, 768);
-    glutDisplayFunc(RenderSceneCB);
+    glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
 
+    GLenum res = glewInit();
+    if (res != GLEW_OK)
+    {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    Vector3f Vertices[3];
+    Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+    glutDisplayFunc(RenderSceneCB);
     glutMainLoop();
 
     return 0;
@@ -22,5 +41,12 @@ int main(int argc, char** argv)
 void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glDrawArrays(GL_POINTS, 0, 1);
+    glDisableVertexAttribArray(0);
+    
     glutSwapBuffers();
 }
